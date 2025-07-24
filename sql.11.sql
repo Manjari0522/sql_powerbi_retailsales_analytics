d44a6aeb99840daad622d6a8e5fd0e5778ac03d1
Manjari0522 <2311701167@stu.manit.ac.in>
2025-07-24 20:28:11 +0530

    Update sql.11.sql

--- sql.11.sql
+++ sql.11.sql
@@ -1,3 +1,5 @@
+-- Order percentage for more than 3,5 and 7 days 
+
 SELECT p.ProductName, p.Index,
 Round(count(distinct o.OrderNumber )*100/(select count( distinct o.OrderNumber) from salesorders as o ), 3) Order_percentage 
 from products as p
@@ -8,4 +10,3 @@ Datediff(str_to_date(ShipDate, "%d-%b-%y"), str_to_date(OrderDate, "%d-%b-%y") )
 Datediff(str_to_date(ShipDate, "%d-%b-%y"), str_to_date(OrderDate, "%d-%b-%y") ) > 7 
 Group by p.Index, p.ProductName 
 order by Order_percentage ASC; 
--- Order percentage for more than 3,5 and 7 days 
