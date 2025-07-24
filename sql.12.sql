bcd19782bc4ac02da16a38c8455c955d63aa96b2
Manjari0522 <2311701167@stu.manit.ac.in>
2025-07-24 20:40:32 +0530

    Update sql.12.sql

--- sql.12.sql
+++ sql.12.sql
@@ -1,3 +1,5 @@
+-- Recency, frequency and monetary of each customer 
+
 SELECT CustomerNameIndex ,
 DATEDIFF(str_to_date('12-jan-20', "%d-%b-%y"), MAX(str_to_date(OrderDate, "%d-%b-%y")) ) as Recency,
 Count( distinct OrderNumber) as Frequency,
@@ -6,4 +8,3 @@ from salesorders
 Group by CustomerNameIndex 
 Order by recency asc, frequency asc, monetary asc ;  
 
--- Recency, frequency and monetary of each customer 
